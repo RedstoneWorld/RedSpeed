e3e0d9e3777e7b2fc56bdd91e0035ab5883104ba
RedstoneFuture <redstonefuture@redstoneworld.de>
2022-11-15 02:58:51 +0100

    Adding comment with permission notes in main class

--- src/main/java/de/redstoneworld/redspeed/RedSpeed.java
+++ src/main/java/de/redstoneworld/redspeed/RedSpeed.java
@@ -2,6 +2,12 @@
  * This is a simple Minecraft Speed-Plugin to allow a changing of
  * own or other user speed for walk and fly.
  *
+ * Permissions:
+ * rwm.redspeed.wspeed.(use|see|set)
+ * rwm.redspeed.wspeed.(see|set).other
+ * rwm.redspeed.fspeed.(use|see|set)
+ * rwm.redspeed.fspeed.(see|set).other
+ *
  * @author Robert Rauh alias RedstoneFuture
  */
 
